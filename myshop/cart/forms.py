c83e32756abb9136fa97f18a12be002794b9e4af
Nikolaj Okulov <kolaokulov50@gmail.com>
2023-09-16 21:02:28 +0300

    adding products in cart and updating quantity products

--- myshop/cart/forms.py
+++ myshop/cart/forms.py
@@ -4,7 +4,7 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoicesField(choices=PRODUCT_QUANTITY_CHOICES,
+    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,
                                        coerce=int)
     override = forms.BooleanField(required=False,
                                   initial=False,
