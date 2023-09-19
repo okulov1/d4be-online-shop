3feb3ed8a29b0f9cab678430db54018b8ff8e2ee
Nikolaj Okulov <kolaokulov50@gmail.com>
2023-09-19 20:15:13 +0300

    edit models and add link to stripe

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Order, OrderItem
+from django.utils.safestring import mark_safe
+
+
+def order_stripe_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ''
+
+order_stripe_payment.short_description = 'Stripe payment'
 
 
 class OrderItemInline(admin.TabularInline):
@@ -11,6 +22,6 @@ class OrderItemInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
+                    order_stripe_payment, 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
