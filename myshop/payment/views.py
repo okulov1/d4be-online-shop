e73f64b3918222d95ada412cfad4a3309c939b23
Nikolaj Okulov <kolaokulov50@gmail.com>
2023-10-12 21:44:20 +0300

    coupons for stripe

--- myshop/payment/views.py
+++ myshop/payment/views.py
@@ -8,6 +8,7 @@ from orders.models import Order
 stripe.api_key = settings.STRIPE_SECRET_KEY
 stripe.api_version = settings.STRIPE_API_VERSION
 
+
 def payment_process(request):
     order_id = request.session.get('order_id', None)
     order = get_object_or_404(Order, id=order_id)
@@ -37,6 +38,15 @@ def payment_process(request):
                 },
                 'quantity': item.quantity,
             })
+
+            if order.coupon:
+                stripe_coupon = stripe.Coupon.create(
+                    name=order.coupon.code,
+                    percent_off=order.discount,
+                    duration='once')
+                session_data['discounts'] = [{
+                    'coupon': stripe_coupon.id
+                }]
 
         session = stripe.checkout.Session.create(**session_data)
 
